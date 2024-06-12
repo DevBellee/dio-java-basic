9b8f4e0ed47de56e788a64634efe1998520a1679
DevBellee <isabelle_melos@hotmail.com>
2024-06-11 21:20:23 -0400

    try and catch class

--- meu-primeiro-projeto-java/CaixaEletronico/src/AboutMe.java
+++ meu-primeiro-projeto-java/CaixaEletronico/src/AboutMe.java
@@ -1,9 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe{
     public static void main(String[] args) {
         //criando o objeto scanner
+        try{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu nome");
@@ -25,9 +27,12 @@ public class AboutMe{
         System.out.println("Minha altura é " + altura + "cm ");
         scanner.close();   
     }
-    
-    
+    catch (InputMismatchException e){
+        System.out.println("Os campos idade e altura precisam ser numéricos");
+    }
 }
+}   
+
 
     
         
